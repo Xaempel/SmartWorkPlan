0721797e6c6f5e304e3321b419325b78225e546f
Xaempel <Xaempel@gmail.com>
2023-11-08 18:12:43 +0100

    <Refactor> -- Frontend folder moved from main project dir to src directory

--- cmake/basic_configuration_functions.cmake
+++ cmake/basic_configuration_functions.cmake
@@ -2,5 +2,5 @@ find_package(Qt6 REQUIRED COMPONENTS Widgets PrintSupport)
 
 function(setup_target TARGET_NAME)
     target_link_libraries(${TARGET_NAME} PUBLIC Qt6::Widgets Qt6::PrintSupport)
-    set_target_properties(${TARGET_NAME} PROPERTIES AUTOUIC_SEARCH_PATHS "./frontend")
+    set_target_properties(${TARGET_NAME} PROPERTIES AUTOUIC_SEARCH_PATHS "./src/frontend")
 endfunction()
